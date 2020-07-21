124513a0856983362d7df64f861c0e41872058ca
Lyuma <xn.lyuma@gmail.com>
2020-07-21 09:37:52 -0700

    Add Docker and kubernetes deployment scripts; use secrets for cockroachdb and elixir keys.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -6,18 +6,25 @@ use Mix.Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
+    System.get_env("COMPILE_PHASE") ||
     raise """
     environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
+    For example: ecto://USER@HOST/DATABASE
     """
 
+database_pass =
+  System.get_env("DATABASE_PASS") ||
+    System.get_env("COMPILE_PHASE")
+
 config :uro, Uro.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
+  password: database_pass,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
+    System.get_env("COMPILE_PHASE") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
