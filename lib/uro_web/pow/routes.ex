b40475b08c918fb0bdda6c7059d86de0d755bd81
Saracen <SaracenOne@gmail.com>
2020-12-18 07:14:32 +0000

    Add underscore to routes

--- lib/uro_web/pow/routes.ex
+++ lib/uro_web/pow/routes.ex
@@ -2,6 +2,6 @@ defmodule UroWeb.Pow.Routes do
   use Pow.Phoenix.Routes
   alias UroWeb.Router.Helpers, as: Routes
 
-  def session_path(conn, verb, query_params \\ []), do: Routes.signin_path(conn, :new)
-  def registration_path(conn, verb, query_params \\ []), do: Routes.signup_path(conn, :new)
+  def session_path(conn, _verb, _query_params \\ []), do: Routes.signin_path(conn, :new)
+  def registration_path(conn, _verb, _query_params \\ []), do: Routes.signup_path(conn, :new)
 end
