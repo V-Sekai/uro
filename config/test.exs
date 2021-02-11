cee5714c862dead339aef135b0cbbc8964899412
Saracen <SaracenOne@gmail.com>
2021-02-11 04:33:54 +0000

    Configure repo for tests

--- config/test.exs
+++ config/test.exs
@@ -2,11 +2,16 @@ use Mix.Config
 
 # Configure your database
 config :uro, Uro.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "uro_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+adapter: Ecto.Adapaters.Postgres,
+username: "root",
+password: "",
+port: "26257",
+database: "uro_test",
+hostname: "localhost",
+show_sensitive_data_on_connection_error: true,
+pool: Ecto.Adapters.SQL.Sandbox,
+pool_size: 10
+
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
