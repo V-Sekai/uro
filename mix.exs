f0f7a96c145025ac7ada26b5ddd57a284fca8502
Saracen <SaracenOne@gmail.com>
2021-04-30 07:07:53 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Uro.MixProject do
     [
       app: :uro,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.11.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers ++ [:phoenix_swagger],
       start_permanent: Mix.env() == :prod,
@@ -33,30 +33,33 @@ defmodule Uro.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5"},
       {:phoenix_pubsub, "~> 2.0"},
+      # Can't currently upgrade these, results in Postgrex error when attempting migration
+      # source SQL: # LOCK TABLE \"schema_migrations\" IN SHARE UPDATE EXCLUSIVE MODE\n^"
       {:phoenix_ecto, "~> 4.2.1"},
       {:ecto_sql, "~> 3.5.3"},
+
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.14.2 "},
+      {:phoenix_html, "~> 2.14 "},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
-      {:gettext, "~> 0.18.2 "},
-      {:hackney, "~> 1.16"},
-      {:jason, "~> 1.2.2 "},
-      {:plug_cowboy, "~> 2.4.1"},
-      {:comeonin, "~> 5.3.1"},
-      {:bcrypt_elixir, "~> 2.2"},
-      {:pow, "~> 1.0.21"},
-      {:email_checker, "~> 0.1.3"},
-      {:pow_assent, "~> 0.4.9"},
+      {:gettext, "~> 0.18 "},
+      {:hackney, "~> 1.17"},
+      {:jason, "~> 1.2 "},
+      {:plug_cowboy, "~> 2.5"},
+      {:comeonin, "~> 5.3.2"},
+      {:bcrypt_elixir, "~> 2.3"},
+      {:pow, "~> 1.0.23"},
+      {:email_checker, "~> 0.1.4"},
+      {:pow_assent, "~> 0.4.10"},
       {:ssl_verify_fun, "~> 1.1.6"},
-      {:phoenix_swagger, "~> 0.8.2"},
+      {:phoenix_swagger, "~> 0.8.3"},
       {:ex_json_schema, "~> 0.7.4"},
-      {:remote_ip, "~> 0.2.1"},
+      {:remote_ip, "~> 1.0"},
       {:waffle, "~> 1.1"},
-      {:waffle_ecto, "~> 0.0.9"},
-      {:swoosh, "~> 1.1"},
-      {:hammer, "~> 6.0"}
+      {:waffle_ecto, "~> 0.0.10"},
+      {:swoosh, "~> 1.3"},
+      {:hammer, "~> 6.0"},
     ]
   end
 
