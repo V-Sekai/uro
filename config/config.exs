d273e8fad7e6d09fa9da40d700c5aa343bdeae2c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-04-10 12:02:55 -0700

    Update import statement in config file

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :hammer,
   backend: {Hammer.Backend.ETS, [expiry_ms: 60_000 * 60 * 4, cleanup_interval_ms: 60_000 * 10]}
